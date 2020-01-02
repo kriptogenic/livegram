2452b860d4f4103e1cb067b5b905ac0ce7ca44c7
kriptonuz <kripton253@gmail.com>
2020-01-02 21:10:20 +0500

    Add function to replyanswerhandelr

--- src/Handlers/ReplyAnswerHandler.php
+++ src/Handlers/ReplyAnswerHandler.php
@@ -28,11 +28,84 @@ class ReplyAnswerHandler extends MessageHandler
             return;
         }
 
+        $this->sendAnswer($message, $user);
+
+
+    }
+
+    private function sendAnswer(Message $message, $user)
+    {
         if (isset($message->text)) {
             $text = $message->text;
             if(isset($message->entities))
                 $text = entityToHtml($message->text, $message->entities);
             $this->api->sendMessage($user, $text);
+            return;
+        }
+
+        if (isset($message->sticker)) {
+            $this->api->sendSticker($user, $message->sticker->fileId);
+            return;
+        }
+
+        if (isset($message->contact)) {
+            $this->api->sendContact(
+                $user,
+                $message->contact->phoneNumber,
+                $message->contact->firstName,
+                $message->contact->lastName ?? null
+            );
+            return;
+        }
+
+        if (isset($message->location)) {
+            $this->api->sendLocation($user, $message->location->latitude, $message->location->longitude);
+            return;
+        }
+
+        if (isset($message->videoNote)) {
+            $this->api->sendVideoNote($user, $message->videoNote->fileId);
+            return;
+        }
+
+        $caption = '';
+        if (isset($message->caption)) {
+            $caption = entityToHtml($message->caption, $message->captionEntities ?? []);
+        }
+
+        if (isset($message->animation)) {
+            $this->api->sendAnimation(
+                $user, $message->animation->fileId,
+                null, null, null, null,
+                $caption,
+                'HTML'
+            );
+            return;
+        }
+
+        if (isset($message->photo)) {
+            $this->api->sendPhoto($user, $message->photo->last()->fileId, $caption, 'HTML');
+            return;
+        }
+
+        if (isset($message->video)) {
+            $this->api->sendVideo(
+                $user,
+                $message->video->fileId,
+                null, null, null, null,
+                $caption,
+                'HTML'
+            );
+            return;
+        }
+
+        if (isset($message->voice)) {
+            $this->api->sendVoice($user, $message->voice->fileId, $caption, 'HTML');
+            return;
+        }
+
+        if (isset($message->document)) {
+            $this->api->sendDocument($user, $message->document->fileId, null, $caption, 'HTML');
             return;
         }
     }
