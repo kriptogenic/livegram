ca4513d7b4eacb648dd51df08fad7624176249d6
kriptonuz <kripton253@gmail.com>
2020-01-06 18:29:45 +0500

    fix start route

--- routes.php
+++ routes.php
@@ -7,7 +7,7 @@ use App\Handlers\PrivateChatHandler;
 use App\Handlers\ReplyAnswerHandler;
 use App\Handlers\StartHandler;
 
-$bot->hears('\/start.*', StartHandler::class);
+$bot->hears('^\/start.*', StartHandler::class);
 $bot->addRoute(ReplyAnswerHandler::class, AdminChatFilter::class, ReplyAnswerFilter::class);
 $bot->addRoute(PrivateChatHandler::class, PrivateChatFilter::class);
 
